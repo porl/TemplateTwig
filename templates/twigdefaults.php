c528d46b1c55d5189ba327ab1763fb9e476331e0
porl <porl42@gmail.com>
2012-06-19 11:29:56 +1000

    Move files to new project

---
+++ templates/twigdefaults.php
@@ -0,0 +1,18 @@
+<?php
+
+$twig_vars['pages'] = $pages;
+$twig_vars['config'] = $config;
+$twig_vars['homepage'] = $pages->get("/");
+
+$twig_vars['page'] = $page;
+
+/**
+ * initialises each field of $page. this is overkill but can be used if you don't want to keep
+ * adding individual fields to the controllers.
+ */
+function twig_init_fields() {
+	foreach ($pages->fields as $field) {
+		$twig_vars['page']->$field = $page->$field;
+		
+	}
+}
