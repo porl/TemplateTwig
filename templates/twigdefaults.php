0b33167cb88236c6014dfd5ac887036b39eea589
porl <porl42@gmail.com>
2012-06-20 12:14:09 +1000

    removed excessive field loader

--- templates/twigdefaults.php
+++ templates/twigdefaults.php
@@ -5,14 +5,3 @@ $twig_vars['config'] = $config;
 $twig_vars['homepage'] = $pages->get("/");
 
 $twig_vars['page'] = $page;
-
-/**
- * initialises each field of $page. this is overkill but can be used if you don't want to keep
- * adding individual fields to the controllers.
- */
-
-
-// foreach ($pages->fields as $field) {
-// 	$twig_vars['page']->$field = $page->$field;
-	
-// }
